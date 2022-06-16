ac46b8504b70f74f3e6db9deeff8ee69d74bd802
afolabiopakunle <opakunleafolabi@gmail.com>
2022-06-16 11:42:14 +0100

    list created successfully

--- README.md
+++
@@ -1,3 +0,0 @@
-# web-platform-jexozt
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-jexozt)
--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('Hello!');
+const mainList = document.getElementById('dragable-list');
+const check = document.getElementById('check');
+
+const richestPeople = [
+  'Afolabi Opakunle',
+  'Jeff Bezos',
+  'Bill Gates',
+  'Warren Buffett',
+  'Bernard Arnault',
+  'Carlos Slim Helu',
+  'Amancio Ortega',
+  'Larry Ellison',
+  'Mark Zuckerberg',
+  'Michael Bloomberg',
+  'Larry Page',
+];
+
+createList();
+
+function createList() {
+  [...richestPeople].forEach((person, index) => {
+    let LI = document.createElement('li');
+    LI.setAttribute('data-index', index);
+    LI.innerHTML = `
+    <span class="number">${index + 1}</span>
+    <div>${person}</div>
+    `;
+
+    mainList.appendChild(LI);
+  });
+}
